cca3e86f01aaf7153cbcf06098959ffa0de30e02
Hoxxy <nikola.karailic@gmail.com>
2022-03-23 00:27:37 +0100

    refactoring

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { WriteProductReviewComponent } from '../../components/write-product-review/write-product-review.component';
 import { Product } from 'src/app/model/product';
@@ -17,41 +17,42 @@ import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+
+export class ProductComponent implements OnInit, OnDestroy {
 
   private sub: any;
   private subC: any;
-  public product: Product = new Product();
-  public category: Category = new Category();
-  
+  product: Product = new Product();
+  category: Category = new Category();
+
   productPurchased: boolean = false;
 
-  quantity: number = 0; 
+  quantity: number = 0;
   productHasReviews: boolean = true;
 
-  constructor(public _MatDialog: MatDialog, 
-    private _MatSnackBar: MatSnackBar,
-    private _ActivatedRoute: ActivatedRoute,
-    private _Router: Router,
-    private _AngularFireAuth: AngularFireAuth,
-    private _AngularFirestore: AngularFirestore,
-    private _ProductService: ProductService,
-    private _CartService: CartService,
-    public _ReviewService: ReviewService,
-    public _AuthService: AuthService,
-    private _CategoryService: CategoryService) { }
+  constructor(public matDialog: MatDialog,
+    private matSnackBar: MatSnackBar,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private angularFireAuth: AngularFireAuth,
+    private angularFirestore: AngularFirestore,
+    private productService: ProductService,
+    private cartService: CartService,
+    private categoryService: CategoryService,
+    public reviewService: ReviewService,
+    public authService: AuthService) { }
 
   ngOnInit(): void {
-    this._ActivatedRoute.params.subscribe(res => {
+    this.activatedRoute.params.subscribe(res => {
       this.getProduct(res.id);
-      this._ReviewService.loadReviews(res.id);
+      this.reviewService.loadReviews(res.id);
 
-      this._AngularFireAuth.authState.subscribe((user) => {
+      this.angularFireAuth.authState.subscribe((user) => {
         if (user) {
-          this._AngularFirestore.collection("orders").ref.where("user", "==", user.uid).where("products", 'array-contains', Number(res.id)).where("status", "==", "Finished").onSnapshot(documents => {
-            if (documents.empty) { 
+          this.angularFirestore.collection("orders").ref.where("user", "==", user.uid).where("products", 'array-contains', Number(res.id)).where("status", "==", "Finished").onSnapshot(documents => {
+            if (documents.empty) {
               this.productPurchased = false;
             }
             else {
@@ -64,42 +65,42 @@ export class ProductComponent implements OnInit {
   }
 
   getProduct = (id: number) => {
-    this.sub = this._ProductService.getProducts('./assets/mock-data/products.json')
-    .subscribe(res => {
-      this.product = res[id-1];
-      this.product.image = this._ProductService.setProductImagePath(this.product);
-      this.getCategoryTitle(this.product.category);
-    })
+    this.sub = this.productService.getProducts('./assets/mock-data/products.json')
+      .subscribe(res => {
+        this.product = res[id - 1];
+        this.product.image = this.productService.setProductImagePath(this.product);
+        this.getCategoryTitle(this.product.category);
+      })
   };
 
   changeQuantity = (newQuantity: number) => {
     this.quantity = newQuantity;
   };
 
-  getCategoryTitle = (id : number) => {
-    this.subC = this._CategoryService.getCategories('./assets/mock-data/categories.json').subscribe(res => {
-      this.category = res[id-1];
-      
+  getCategoryTitle = (id: number) => {
+    this.subC = this.categoryService.getCategories('./assets/mock-data/categories.json').subscribe(res => {
+      this.category = res[id - 1];
+
     });
   }
 
   public addToCart = (product: Product) => {
 
     if (this.quantity <= 0) this.quantity = 1;
-    this._CartService.addToCart({ product, quantity:this.quantity });
+    this.cartService.addToCart({ product, quantity: this.quantity });
 
-    this._MatSnackBar
-    .open("Added to the shopping cart.", "View cart", { duration: 4000 })
-    .afterDismissed().subscribe(info => {
-      if (info.dismissedByAction === true) {
-        this._Router.navigate(['/cart']);
-      }
-    });
+    this.matSnackBar
+      .open("Added to the shopping cart.", "View cart", { duration: 4000 })
+      .afterDismissed().subscribe(info => {
+        if (info.dismissedByAction === true) {
+          this.router.navigate(['/cart']);
+        }
+      });
 
   };
 
   openReviewDialog = () => {
-    this._MatDialog.open(WriteProductReviewComponent, {
+    this.matDialog.open(WriteProductReviewComponent, {
       data: {
         productPurchased: this.productPurchased,
         productId: this.product.id
