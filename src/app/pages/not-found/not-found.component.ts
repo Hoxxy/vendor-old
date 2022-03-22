a0b47f7973f3adb3c5697d83e8d3f4c1118dd886
Hoxxy <nikola.karailic@gmail.com>
2022-03-23 00:24:41 +0100

    refactoring

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
-})
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  template: `<p>404 Page Not Found</p>`,
+  styles: [`
+  p {
+    text-align: center;
+    font-size: x-large;
+    font-weight: bolder;
+    padding: 200px;
   }
-
+`]
+})
+export class NotFoundComponent {
+  constructor() { }
 }
