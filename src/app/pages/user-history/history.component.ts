7f4acbca20c7620212f220aa7127e95bdf95d790
Hoxxy <nikola.karailic@gmail.com>
2022-03-23 00:30:50 +0100

    refactoring

--- src/app/pages/user-history/history.component.ts
+++ src/app/pages/user-history/history.component.ts
@@ -13,8 +13,8 @@ import { ProductService } from 'src/app/shared/services/product.service';
   styleUrls: ['./history.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
@@ -25,20 +25,19 @@ export class UserHistoryComponent implements OnInit {
   public orders: Array<Order> = [];
 
   constructor(
-    private _AngularFirestore: AngularFirestore,
-    private _AngularFireAuth: AngularFireAuth,
-    private _ProductService: ProductService
+    private angularFirestore: AngularFirestore,
+    private angularFireAuth: AngularFireAuth,
+    private productService: ProductService
   ) { }
-  
+
 
   ngOnInit(): void {
-
     var productList: Array<Product>;
-    this.subP = this._ProductService.getProducts('./assets/mock-data/products.json').subscribe(res => {
+    this.subP = this.productService.getProducts('./assets/mock-data/products.json').subscribe(res => {
       productList = res as Array<Product>;
 
       productList.forEach(product => {
-        product.image = this._ProductService.setProductImagePath(product);
+        product.image = this.productService.setProductImagePath(product);
       })
 
       this.getUserOrders().then(response => {
@@ -56,12 +55,12 @@ export class UserHistoryComponent implements OnInit {
             "postcode": order.postcode,
             "products": order.products,
             "quantity": order.quantity,
-            "productInfo": productList.filter(({id}) => order.products.includes(id))
+            "productInfo": productList.filter(({ id }) => order.products.includes(id))
           });;
         });
         this.dataSource.data = this.orders;
       });
-    }); 
+    });
   }
 
   displayedColumns: string[] = ['id', 'displayname', 'date', 'status', 'total', 'actions'];
@@ -71,16 +70,16 @@ export class UserHistoryComponent implements OnInit {
 
   private getUserOrders(): Promise<Array<Order>> {
     return new Promise((resolve, reject) => {
-      this._AngularFireAuth.authState.subscribe((user) => {
+      this.angularFireAuth.authState.subscribe((user) => {
         if (user) {
-          this._AngularFirestore.collection("orders").ref.where("user", "==", user.uid).onSnapshot(documents => {
-            if (documents.empty) { 
-              resolve([]) 
+          this.angularFirestore.collection("orders").ref.where("user", "==", user.uid).onSnapshot(documents => {
+            if (documents.empty) {
+              resolve([])
             }
             else {
-              resolve(documents.docs.reduce((output, document) => { 
+              resolve(documents.docs.reduce((output, document) => {
                 output.push(document.data() as Order);
-                return output; 
+                return output;
               }, Array<Order>()))
             }
           }, error => reject(error));
@@ -88,7 +87,7 @@ export class UserHistoryComponent implements OnInit {
       });
     });
   }
-  
+
   ngOnDestroy = () => {
     this.subP.unsubscribe();
   }
